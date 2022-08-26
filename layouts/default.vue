5f957d11610f9f97370dc1e865f6486550555824
Victor FERNANDEZ <victor.fernandez@crystalgroup.fr>
2022-08-26 18:01:43 +0200

    Add mongoDB database

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,25 @@
     <div>
         <b-navbar variant="dark" type="dark">
             <b-navbar-nav>
-                <b-nav-item @click="goTo('index')">Acceuil</b-nav-item>
-                <b-nav-item @click="goTo('games')">Jeux</b-nav-item>
+                <b-nav-item @click="goTo('index')" :active="nameRoute === 'index'">Acceuil</b-nav-item>
+                <b-nav-item @click="goTo('games')" :active="nameRoute === 'games'">Jeux</b-nav-item>
             </b-navbar-nav>
         </b-navbar>
         <Nuxt/>
     </div>
 </template>
-<script setup>
-const goTo = (name) => { $nuxt.$router.push({name}) }
+
+<script>
+export default {
+    computed: {
+        nameRoute() {
+            return this.$route.name
+        }
+    },
+    methods: {
+        goTo(name) {
+            this.$router.push({name})
+        }
+    }
+}
 </script>
--- server/index.js
+++ server/index.js
@@ -1,18 +1,12 @@
 import express from 'express'
 import { Router } from 'express';
+import { MongoClient } from 'mongodb';
+import dbConfig from './config/database'
 
 const app = express();
 const router = Router();
 
 app.use(router);
-
-router.get('/test', (req, res) => {
-    console.log("Je suis passé ici")
-    res.json({
-        test: "toto",
-        test2: "titi"
-    })
-})
 
 module.exports=  {
     path: '/api',
