05354bf58239850ad82b9c78eeaab2ddd9355006
Victor FERNANDEZ <victorfernandez@hotmail.fr>
2022-09-01 21:38:30 +0200

    Add $router, $route and $store to Vue TS config

--- server/types/vue-shim.d.ts
+++ server/types/vue-shim.d.ts
@@ -1,4 +1,16 @@
+import Vue from "vue";
+import VueRouter from 'vue-router';
+import {Route} from 'vue-router';
+import {Store} from 'vuex';
 declare module "*.vue" {
-    import Vue from 'vue'
+    import Vue from 'vue';
     export default Vue
+}
+
+declare module "vue/types/vue" {
+    interface Vue {
+        $router: VueRouter,
+        $route: Route,
+        $store: Store<any>
+    }
 }
